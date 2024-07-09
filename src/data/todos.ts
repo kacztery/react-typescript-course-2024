51587a3f6739779fa695988f302fb97cf18bb2a1
Kacper Kupczak <kacper.kupczak@gmail.com>
2024-07-09 11:30:33 +0200

    Cleaning default Vite project

---
+++ src/data/todos.ts
@@ -0,0 +1,17 @@
+export const dummyData = [
+    {
+        id: 1,
+        title: 'Like this video. 👍🏻',
+        completed: false,
+    },
+    {
+        id: 2,
+        title: 'Subscribe to Coding in Flow. 📺',
+        completed: false,
+    },
+    {
+        id: 3,
+        title: 'Leave a nice comment. 😊',
+        completed: false,
+    },
+];
